58bb4a2f238e08347c5a87f353f42a8bfa42c38b
Peter <schmidt.multimedia@gmail.com>
2024-05-27 09:29:56 +0200

    01a / Refactoring ohne extends

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,49 +2,32 @@ package com.cc.java;
 
 public class Cat {
 
-  private String name;
-  private String furColor;
-  private int age; 
+    private int counter;
 
-  private int counter;
+    public Cat(String name, String furColor, int age) {
+      
+    }
 
-public Cat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
-  }
 
-public String getStringAttributes(String op) {
-  switch (op) {
-    case "#name":
-      return name;
-    case "#color":
-      return furColor;
-    default:
-      return "ERROR!";
-  }
-}
+    public String getAge() {
+        return checkCompliance();
+    }
 
-public String getAge() {
-    return checkCompliance();
-}
+    private String checkCompliance(){
 
-private String checkCompliance(){
+      counter++;
 
-  counter++;
-
-  switch (counter) {
-    case 1:
-      return "This is an inappropriate question!"; // 1st time
-    case 2:
-      return "I've told you once!"; // 2nd time
-    case 3:
-      return "Talk to the hand!"; // 3rd time
-    default:
-      return "1#!?&&%"; //4th ++
-  }
-  
-}
-
+      switch (counter) {
+        case 1:
+          return "This is an inappropriate question!"; // 1st time
+        case 2:
+          return "I've told you once!"; // 2nd time
+        case 3:
+          return "Talk to the hand!"; // 3rd time
+        default:
+          return "1#!?&&%"; //4th ++
+      }
+      
+    }
 }
   
